28c244e7afdb3db9a69915e2b2ce29198471de02
tilav-web <shavqiddintilovov0@gmail.com>
2025-06-30 14:29:33 +0500

    some changed

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -4,9 +4,9 @@ import { Outlet } from "react-router-dom";
 
 export default function RootLayout() {
   return (
-    <div>
+    <div className="flex flex-col h-screen">
       <Header />
-      <main className="p-4 py-24">
+      <main className="flex-1 container mx-auto p-4 pt-20 pb-20 overflow-y-auto">
         <Outlet />
       </main>
       <Footer />
