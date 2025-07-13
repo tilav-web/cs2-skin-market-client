d72bd2e95dfe49ed9e6af81b965602398f5ca76b
tilav-web <shavqiddintilovov0@gmail.com>
2025-07-13 21:17:23 +0500

    .

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -3,8 +3,8 @@ import { privateInstance } from "@/common/api/client-api";
 export const transactionService = {
   getUserTransactions: async () => {
     try {
-      const transactions = await privateInstance.get("/transactions/my");
-      return transactions;
+      const res = await privateInstance.get("/transactions/my");
+      return res.data;
     } catch (error) {
       console.error("Error fetching user transactions:", error);
       throw error;
